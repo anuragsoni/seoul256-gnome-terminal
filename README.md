c0b49ec02e6b0d53b24838dbf10816c8d52a81a2
Anurag Soni <anuragsoni.13@gmail.com>
2015-06-11 22:41:30 -0400

    seoul256-dark gnome-terminal color scheme

---
+++ README.md
@@ -0,0 +1,12 @@
+# seol256-gnome-terminal
+
+A port of the seol256-dark [vim theme](https://github.com/junegunn/seoul256.vim)
+
+## To install on ubuntu/ubuntu based distros:
+
+```
+git clone https://github.com/anuragsoni/seoul256-gnome-terminal.git ~/.config/seoul256-gnome-terminal
+source ~/.config/seol256-gnome-terminal/seoul-256-gnome-terminal.sh
+```
+
+Next, restart or open Gnome terminal. Right click on the terminal to select the scheme you just sourced.
---
+++ seoul256-dark.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+# Seol256 - Gnome Terminal color scheme install script
+# Anurag Soni
+
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Seol256"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="seol256"
+[[ -z "$DCONF" ]] && DCONF=dconf
+[[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen
+
+dset() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    if [[ "$type" == "string" ]]; then
+        val="'$val'"
+    fi
+
+    "$DCONF" write "$PROFILE_KEY/$key" "$val"
+}
+
+# because dconf still doesn't have "append"
+dlist_append() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+        {
+            "$DCONF" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+            echo "'$val'"
+        } | head -c-1 | tr "\n" ,
+    )"
+
+    "$DCONF" write "$key" "[$entries]"
+}
+
+# Newest versions of gnome-terminal use dconf
+if which "$DCONF" > /dev/null 2>&1; then
+    [[ -z "$BASE_KEY_NEW" ]] && BASE_KEY_NEW=/org/gnome/terminal/legacy/profiles:
+
+    if [[ -n "`$DCONF list $BASE_KEY_NEW/`" ]]; then
+        if which "$UUIDGEN" > /dev/null 2>&1; then
+            PROFILE_SLUG=`uuidgen`
+        fi
+
+        if [[ -n "`$DCONF read $BASE_KEY_NEW/default`" ]]; then
+            DEFAULT_SLUG=`$DCONF read $BASE_KEY_NEW/default | tr -d \'`
+        else
+            DEFAULT_SLUG=`$DCONF list $BASE_KEY_NEW/ | grep '^:' | head -n1 | tr -d :/`
+        fi
+
+        DEFAULT_KEY="$BASE_KEY_NEW/:$DEFAULT_SLUG"
+        PROFILE_KEY="$BASE_KEY_NEW/:$PROFILE_SLUG"
+
+        # copy existing settings from default profile
+        $DCONF dump "$DEFAULT_KEY/" | $DCONF load "$PROFILE_KEY/"
+
+        # add new copy to list of profiles
+        dlist_append $BASE_KEY_NEW/list "$PROFILE_SLUG"
+
+        # update profile values with theme options
+        dset visible-name "'$PROFILE_NAME'"
+        dset palette "['#4E4E4E', '#D68787', '#5F865F', '#D8AF5F', '#85ADD4', '#D7AFAF', '#87AFAF', '#D0D0D0', '#626262', '#D75F87', '#87AF87', '#FFD787', '#ADD4FB', '#FFAFAF', '#87D7D7', '#E4E4E4']"
+        dset background-color "'#3A3A3A'"
+        dset foreground-color "'#D0D0D0'"
+        dset bold-color "'#E4E4E4'"
+        dset bold-color-same-as-fg "false"
+        dset use-theme-colors "false"
+        dset use-theme-background "false"
+
+        unset PROFILE_NAME
+        unset PROFILE_SLUG
+        unset DCONF
+        unset UUIDGEN
+        exit 0
+    fi
+fi
+
+# Fallback for Gnome 2 and early Gnome 3
+[[ -z "$GCONFTOOL" ]] && GCONFTOOL=gconftool
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/apps/gnome-terminal/profiles
+
+PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
+
+gset() {
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
+
+    "$GCONFTOOL" --set --type "$type" "$PROFILE_KEY/$key" -- "$val"
+}
+
+# Because gconftool doesn't have "append"
+glist_append() {
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+        {
+            "$GCONFTOOL" --get "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+            echo "$val"
+        } | head -c-1 | tr "\n" ,
+    )"
+
+    "$GCONFTOOL" --set --type list --list-type $type "$key" "[$entries]"
+}
+
+# Append the Base16 profile to the profile list
+glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
+
+gset string visible_name "$PROFILE_NAME"
+gset string palette "#4E4E4E:#D68787:#5F865F:#D8AF5F:#85ADD4:#D7AFAF:#87AFAF:#D0D0D0:#626262:#D75F87:#87AF87:#FFD787:#ADD4FB:#FFAFAF:#87D7D7:#E4E4E4"
+gset string background_color "#3A3A3A"
+gset string foreground_color "#D0D0D0"
+gset string bold_color "#E4E4E4"
+gset bool   bold_color_same_as_fg "false"
+gset bool   use_theme_colors "false"
+gset bool   use_theme_background "false"
+
+unset PROFILE_NAME
+unset PROFILE_SLUG
+unset DCONF
+unset UUIDGEN
